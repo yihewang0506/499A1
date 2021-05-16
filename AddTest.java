13090b9608c8758506043c858b945110290bed23
yihewang0506 <77508297+yihewang0506@users.noreply.github.com>
2021-05-16 15:57:13 -0700

    addTest update

--- AddTest.java
+++ AddTest.java
@@ -1,3 +1,29 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class AddTest {
-    @test
+	private static Calculator Cal1 = null;
+
+	@BeforeClass
+	public static void setup() {
+		Cal1 = new calculator();
+	}
+
+	@Test
+	public void Test1() {
+		double actual = Cal1.add(3.4, 2.5);
+		double expected = 5.9;
+		assertEquals("Add function is correct", expected, actual, 0);
+	}
+
+	@Test
+	public void Test2() {
+		double actual = Cal1.add(3.6, 2.4);
+		double expected = 6;
+		assertEquals("Add function is correct", expected, actual, 0);
+	}
+
 }

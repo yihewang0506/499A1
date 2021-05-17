8686b24e2076c200733f30b1a3e9adf9c8828a74
yihewang0506 <77508297+yihewang0506@users.noreply.github.com>
2021-05-16 17:16:06 -0700

    update mainpage
    change the sub function and write some comments

--- AddTest.java
+++ AddTest.java
@@ -16,14 +16,14 @@ public class AddTest {
 	public void Test1() {
 		double actual = Cal1.add(3.4, 2.5);
 		double expected = 5.9;
-		assertEquals("Add function is correct", expected, actual, 0);
+		assertEquals(expected, actual, 0);
 	}
 
 	@Test
 	public void Test2() {
 		double actual = Cal1.add(3.6, 2.4);
 		double expected = 6;
-		assertEquals("Add function is correct", expected, actual, 0);
+		assertEquals(expected, actual, 0);
 	}
 
 }
--- asd/src/calculator.java
+++ asd/src/calculator.java
@@ -2,19 +2,19 @@ package asd.src;
 
 
 public class calculator {
-
+	//this is add function
 	public double add(double a, double b) {
 		return a + b;
 	}
-
+	//this is sub function
 	public double substract(double a, double b) {
-		return a + b;
+		return a - b;
 	}
-
+	// this is mul function
 	public double mutiply(double a, double b) {
 		return a * b;
 	}
-
+	//this is divide function
 	public double divide(double a, double b) {
 		if (b == 0) {
 			throw new IllegalArgumentException("Donot divide by 0");
